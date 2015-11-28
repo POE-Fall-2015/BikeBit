50e18d531486be0ee175935527fd421b6b56e3de
Dennis Chen <dennis.chen@students.olin.edu>
2015-11-28 18:54:27 -0500

    graphing on dashboard now works

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -18,96 +18,112 @@
     $('.header-content-inner-number').text(data.distToGo);
   });
 
+  //holds the bar chart object
+  var myBarChart;
 
-    
-  Chart.defaults.global.responsive = true;
-  //extend the bar graph prototype to get behavior where we
-  //can overlay a horizontal line of our own
-  Chart.types.Bar.extend({
-      name: "BarWithLine",
-      initialize: function () {
-          Chart.types.Bar.prototype.initialize.apply(this, arguments);
-      },
-      draw: function () {
-          Chart.types.Bar.prototype.draw.apply(this, arguments);
-          var scale = this.scale
-          var linePos = scale.calculateY(this.options.lineAtPos);
+  //graphTime variable keeps track of the week that
+  //graph currently is showing
+  var date = new Date();
+  var graphTime = date.getTime();
+  var week = 1000*60*60*24*7; //ms per week
 
-          // draw line
-          this.chart.ctx.beginPath();
-          this.chart.ctx.moveTo(scale.startPoint+12, linePos);
-          this.chart.ctx.strokeStyle = '#000';
-          this.chart.ctx.lineTo(this.chart.width, linePos);
-          this.chart.ctx.stroke();
-          
-          // write label for line
-          this.chart.ctx.textAlign = 'center';
-          var lineLabel = this.options.lineLabel;
-          this.chart.ctx.fillStyle = '#000000';
-          this.chart.ctx.fillText(lineLabel, scale.startPoint + 60, linePos-15);
-      }
-  });
+  //click handler for clicking to see previous week's graph
+  var getPrevGraph = function(){
+    graphTime -= week;
+    $.get("/graphData", {graphTime:graphTime}, function(graphData){
+      drawGraph(graphData);
+    });
+  };
+
+  //click handler for clicking to see next week's graph
+  var getNextGraph = function(){
+    graphTime += week;
+    $.get("/graphData", {graphTime:graphTime}, function(graphData){
+      drawGraph(graphData);
+    });
+  };
+
+  function drawGraph(graphData){
+    if(myBarChart != null){
+      myBarChart.destroy();
+    };
+    //draw bar graph
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var data = {
+        labels: graphData.xlabels,
+        datasets: [
+            {
+                label: graphData.goalLabel,
+                fillColor: "#eb3812",
+                strokeColor: "#eb3812",
+                pointStrokeColor: "#000",
+                pointHighlightFill: "#000",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: graphData.ys
+            },
+        ]
+    };
+    var options = {
+      scaleStartValue: 0,
+      scaleFontFamily : "'Open Sans','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+      scaleFontColor: "#000",
+      scaleFontSize: 15,
+      pointLabelFontFamily : "'Open Sans','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+      pointLabelFontColor: "#000",
+      scaleFontSize: 15,
+      lineAtPos: graphData.goal,
+      lineLabel: graphData.goalLabel
+    };
+    myBarChart = new Chart(ctx).BarWithLine(data, options);
+    $('#chartTitle').html(graphData.graphTitle);
+  };
+
+  function initializeGraph(graphData){
+    //initial setup
+    Chart.defaults.global.responsive = true;
+    //extend the bar graph prototype to get behavior where we
+    //can overlay a horizontal line of our own
+    Chart.types.Bar.extend({
+        name: "BarWithLine",
+        initialize: function () {
+            Chart.types.Bar.prototype.initialize.apply(this, arguments);
+        },
+        draw: function () {
+            Chart.types.Bar.prototype.draw.apply(this, arguments);
+            var scale = this.scale
+            var linePos = scale.calculateY(this.options.lineAtPos);
+            // draw line
+            this.chart.ctx.beginPath();
+            this.chart.ctx.moveTo(scale.startPoint+20, linePos);
+            this.chart.ctx.strokeStyle = '#000000';
+            this.chart.ctx.lineTo(this.chart.width, linePos);
+            this.chart.ctx.stroke();
+            this.chart.ctx.textAlign = 'center';
+            // goal line may or may not be visible depending on the
+            // scale of the graph
+            var goalVisible = this.options.lineAtPos <= this.scale.max;
+            if(goalVisible){
+              // draw label next to goal line
+              var lineLabel = this.options.lineLabel;
+              this.chart.ctx.fillStyle = '#000000';
+              this.chart.ctx.fillText(lineLabel, scale.startPoint + 80, linePos+15);
+            } else {
+              // draw label in specific place
+              var lineLabel = this.options.lineLabel;
+              this.chart.ctx.fillStyle = '#000000';
+              this.chart.ctx.fillText(lineLabel, scale.startPoint + 150, 30);
+            }
+        }
+    });
+    drawGraph(graphData);
+    //create event handlers for left and right buttons
+    $('#chartLeft').click(getPrevGraph);
+    $('#chartRight').click(getNextGraph);
+  };
 
   //get json object (faked right now for testing)
-  var graphData = {
-    xlabels: ["9/08","9/09","9/10","9/11","9/12","9/13","9/14"],
-    ys: [0,4,6,2,1,9,11],
-    goal: 50,
-    goalLabel: "Your weekly target",
-    graphTitle: "miles biked daily",
-    //week begin time in terms of date.getMilliSeconds()
-    weekBeginTime: 901238902130
-  };
-
-  //create bar graph
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var data = {
-      labels: graphData.xlabels,
-      datasets: [
-          {
-              label: graphData.goalLabel,
-              fillColor: "#eb3812",
-              strokeColor: "#eb3812",
-              pointStrokeColor: "#000",
-              pointHighlightFill: "#000",
-              pointHighlightStroke: "rgba(220,220,220,1)",
-              data: graphData.ys
-          },
-      ]
-  };
-  var scaleOverride = false;
-  if(graphData.goal > Math.max.apply(Math, graphData.ys)){
-    //override the scale in order to show the goal line
-    scaleOverride = true;
-  }
-  var options = {     
-    // Boolean - If we want to override with a hard coded scale
-    scaleOverride: scaleOverride,
-    // ** Required if scaleOverride is true **
-    // Number - The number of steps in a hard coded scale
-    scaleSteps: 8,
-    // Number - The value jump in the hard coded scale
-    scaleStepWidth: Math.floor(graphData.goal / 7),
-    // Number - The scale starting value
-    scaleStartValue: 0,
-    scaleFontFamily : "'Open Sans','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-    scaleFontColor: "#000",
-    scaleFontSize: 15,
-    pointLabelFontFamily : "'Open Sans','Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-    pointLabelFontColor: "#000",
-    scaleFontSize: 15,
-    lineAtPos: graphData.goal,
-    lineLabel: "weekly goal"
-  };
-  var myBarChart = new Chart(ctx).BarWithLine(data, options);
-
-  //create event handlers for left and right buttons
-  $('#chartLeft').click(function(){
-    console.log('left clicked!');
-  });
-
-  $('#chartRight').click(function(){
-    console.log('right clicked!');
+  $.get("/graphData", {graphTime:graphTime}, function(graphData){
+    initializeGraph(graphData);
   });
 
 })(jQuery); // End of use strict
